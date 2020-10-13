3cf39749078d120e3acb6fda8856812da6629446
c-sinner <claude.sinner@utdallas.edu>
2020-10-13 14:30:17 -0500

    lowered python version

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='sniphles',
           'numpy',
       ],
       packages=find_packages(exclude=('tests',)),
-      python_requires='>=3.8',
+      python_requires='>=3.6',
       zip_safe=False)
