2da1931ec604e0ac9fb6317b44d293cfd5b641e1
idraktt <idraktt@gmail.com>
2020-10-14 03:18:14 +0300

    Update update_sv_hp_ps_group_1.py

--- update_sv_hp_ps_group_1.py
+++ update_sv_hp_ps_group_1.py
@@ -93,22 +93,10 @@ def update_vcf(args):
                 if line_split[-1].split(":", 1)[0] == "1/1" or line_split[-1].split(":", 1)[0] == "0/0" or line_split[-1].split(":", 1)[0] == "./.":  # no gt to phase
                     reads = [i for i in line_split[7].split(";")  if i.startswith("RNAMES")][0].split("=",1)[-1].split(",")                  
                     myvalues = list(map(hp_dic.get, reads))
-                    total_count = len(myvalues)
-                    count_of_1s = 0
-                    count_of_2s = 0
-                    count_of_None = 0
                     if any(myvalues): # any value is not none
                         ps_dict = categorize_ps(myvalues)
                         if 0 in list(ps_dict.values()): # means that the hp is conflicting do not update anything and add flag that is is conflicting
-                            for i in myvalues:
-                                if i is not None:
-                                    if i[0] == '1':
-                                        count_of_1s+=1
-                                    elif i[0] == '2':
-                                        count_of_2s+=1
-                                else:
-                                    count_of_None=+1
-                            ratios = count_of_1s,count_of_2s,count_of_None
+                            ratios = collect_ratios(myvalues)
                             line_split[7] = "{info};CONFLICT={conflict};HP_RATIO={hp_ratio}".format(info=line_split[7], conflict=0, hp_ratio=ratios)
                             line_split[-2] = "{}:{}".format(line_split[-2], "PS")
                             line_split[-1] = "{}:{}".format(line_split[-1], "-1")
@@ -117,38 +105,18 @@ def update_vcf(args):
                     reads = [i for i in line_split[7].split(";")  if i.startswith("RNAMES")][0].split("=",1)[-1].split(",")
                     myvalues = list(map(hp_dic.get, reads))  # list of lists first element id hp second is ps or None in case there are no reads with hp and ps to support this sv
                     # If any value not None
-                    total_count = len(myvalues)
-                    count_of_1s = 0
-                    count_of_2s = 0
-                    count_of_None = 0
                     if any(myvalues): # any value is not none
                         ps_dict = categorize_ps(myvalues)
                         if 0 in list(ps_dict.values()): # means that the hp is conflicting do not update anything and add flag that is is conflicting
-                            for i in myvalues:
-                                if i is not None:
-                                    if i[0] == '1':
-                                        count_of_1s+=1
-                                    elif i[0] == '2':
-                                        count_of_2s+=1
-                                else:
-                                    count_of_None=+1
-                            ratios = count_of_1s,count_of_2s,count_of_None
+                            ratios = collect_ratios(myvalues)
                             line_split[7] = "{info};CONFLICT={conflict};HP_RATIO={hp_ratio}".format(info=line_split[7], conflict=1, hp_ratio=ratios)
 
                             line_split[-2] = "{}:{}".format(line_split[-2], "PS")
                             line_split[-1] = "{}:{}".format(line_split[-1], ",".join(ps_dict.keys()))
                             data_out.write("{}\n".format("\t".join(line_split)))
                         else: # update the gt field and ps to sv
-                            for i in myvalues:
-                                if i is not None:
-                                    if i[0] == '1':
-                                        count_of_1s+=1
-                                    elif i[0] == '2':
-                                        count_of_2s+=1
-                                else:
-                                    count_of_None=+1
-                            ratios = count_of_1s,count_of_2s,count_of_None
-                            line_split[7] = "{info};CONFLICT={conflict};HP_RATIO={hp_ratio}".format(info=line_split[7], conflict=1, hp_ratio=ratios)
+                            ratios = collect_ratios(myvalues)
+                            line_split[7] = "{info};CONFLICT={conflict};HP_RATIO={hp_ratio}".format(info=line_split[7], conflict=0, hp_ratio=ratios)
                             line_split[-2] = "{}:{}".format(line_split[-2], "PS")
                             # if values are negative then it is hp=1 1|0 else it is hp2 0|1
                             # line_split[-1] = line_split[-1].replace("/", "|")
@@ -195,6 +163,21 @@ def categorize_ps(myvalues):
                 ps_dict[ps] = 1
     return ps_dict
 
+def collect_ratios(myvalues):
+    count_of_1s = 0
+    count_of_2s = 0
+    count_of_None = 0
+    collected_ratios = 0,0,0
+    for i in myvalues:
+        if i is not None:
+            if i[0] == '1':
+                count_of_1s+=1
+            elif i[0] == '2':
+                count_of_2s+=1
+        else:
+            count_of_None=+1
+        collected_ratios = count_of_1s,count_of_2s,count_of_None
+    return collected_ratios
 
 def main():
     args = get_args()
